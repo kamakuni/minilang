ffaf299f0001a094f6939b58c5d7e1ddb98971ce
kamakuni <superradio86@gmail.com>
2018-03-17 23:51:15 +0900

    implementing codes for function applicaton

--- main.go
+++ main.go
@@ -18,14 +18,14 @@ var cnt int
 var fn = map[rune]string{}
 
 func error(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a)
+	//fmt.Fprintf(os.Stderr, format, a)
 	os.Exit(1)
 }
 
 func expect(r rune) {
 	fmt.Printf("rune:%d", r)
 	if prg[cnt] != r {
-		fmt.Printf("rune:%d", prg[cnt])
+		//fmt.Printf("rune:%d", prg[cnt])
 		error("%c expected: %s", prg[cnt])
 	}
 	cnt++
@@ -33,17 +33,10 @@ func expect(r rune) {
 
 func readUtil(r rune, name rune, target map[rune]string) {
 	var buf = ""
-	fmt.Println("*******************************")
 	for cnt < len(prg) && prg[cnt] != r {
-		fmt.Printf("prg:%s\n", string(prg[cnt]))
-		fmt.Printf("r:%s\n", string(r))
 		buf = buf + string(prg[cnt])
 		cnt++
-		fmt.Printf("cnt:%d\n", cnt)
 	}
-	fmt.Println("*******************************")
-	fmt.Printf("function name:%s\n", string(name))
-	fmt.Printf("function:%s\n", buf)
 	target[name] = buf
 	cnt++
 }
@@ -56,6 +49,7 @@ func skip() {
 
 func evalString(code string, arg int) int {
 	orig := prg
+	prg = []rune(code)
 	origCnt := cnt
 	cnt = 0
 	val := eval(arg)
@@ -69,29 +63,22 @@ func eval(arg int) int {
 
 	// Function paramter
 	if cnt < len(prg) && prg[cnt] == '.' {
-		fmt.Println("Function param")
 		cnt++
 		return arg
 	}
 
 	// Function definition
-	if cnt < len(prg) && unicode.IsUpper(prg[cnt]) && prg[1] == '[' {
+	if cnt < len(prg) && unicode.IsUpper(prg[cnt]) && prg[cnt+1] == '[' {
 		name := prg[cnt]
 		cnt += 2
 		readUtil(']', name, fn)
 		return eval(arg)
 	}
-	fmt.Println("---------------------")
-	fmt.Printf("prg:%s\n", string(prg[cnt]))
-	fmt.Printf("cnt:%d\n", string(prg[cnt+1]))
-	fmt.Println("---------------------")
+
 	// Function application
 	if cnt < len(prg) && unicode.IsUpper(prg[cnt]) && prg[cnt+1] == '(' {
-		fmt.Printf("func apply:%d\n", cnt)
 		name := prg[cnt]
-		fmt.Printf("name:%d\n", name)
 		cnt += 2
-		fmt.Printf("cnt3:%d\n", cnt)
 		newarg := eval(arg)
 		expect(')')
 		return evalString(fn[name], newarg)
@@ -131,7 +118,8 @@ func eval(arg int) int {
 }
 
 func main() {
-	prg = []rune(os.Args[1])
+	//prg = []rune(os.Args[1])
+	prg = []rune("F[+ . .] F(1)")
 	for cnt < len(prg) {
 		fmt.Printf("%d\n", eval(0))
 	}
